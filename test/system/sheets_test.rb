2c5500b0c42c1096199845f607a5fa460536409c
nishide <nishide.h@gmail.com>
2018-09-11 15:52:47 +0900

    add Sheetのシステムテスト追加

--- test/system/sheets_test.rb
+++ test/system/sheets_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class SheetsTest < ApplicationSystemTestCase
   setup do
     @sheet = sheets(:one)
+    page.driver.clear_memory_cache
   end
 
   test "visiting the index" do
@@ -19,6 +20,17 @@ class SheetsTest < ApplicationSystemTestCase
 
     assert_text "Sheet was successfully created"
     click_on "Back"
+  end
+  
+  test "登録したシート確認" do
+    # visit sheet_url(id: @sheet.id)
+    visit sheets_url
+    assert_equal 200, status_code
+
+    all(:css, 'tbody tr')[1].click_link "Show"
+    assert_equal 200, status_code
+    
+    assert_text @sheet.title
   end
 
   test "updating a Sheet" do
