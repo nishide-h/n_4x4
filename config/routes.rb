6266a21475bde5c40704ef80a060943082217ad3
nishide <nishide.h@gmail.com>
2018-09-11 15:49:00 +0900

    update rootパスはシート一覧

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'sheets#index'
   resources :sheets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
