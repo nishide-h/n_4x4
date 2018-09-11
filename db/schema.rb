c03e2faa2e9bdfc6ab31826d035fdfb8e0189c79
nishide <nishide.h@gmail.com>
2018-09-11 17:05:20 +0900

    add Tasks scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_075521) do
+ActiveRecord::Schema.define(version: 2018_09_11_075745) do
 
   create_table "sheets", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "sheet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sheet_id"], name: "index_tasks_on_sheet_id"
   end
 
 end
