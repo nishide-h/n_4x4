a92cb9b3e8165b2fd535f4edd431f7484f43f619
nishide <nishide.h@gmail.com>
2018-11-19 18:03:52 +0900

    update task追加にともない、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root 'sheets#index'
-  resources :tasks
-  resources :sheets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "sheets#index"
+  resources :sheets do
+    resources :tasks
+  end
 end
