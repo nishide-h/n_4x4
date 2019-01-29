83be0e43465daf1f8d3ac858af15498157800077
nishide <nishide.h@gmail.com>
2019-01-29 22:08:25 +0900

    Add #16 task,sheetのModelテスト

---
+++ spec/models/sheet_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Sheet, type: :model do
+  before do
+    @sheet = FactoryBot.create(:sheet)
+  end
+
+  it "is valid" do
+    expect(@sheet).to be_valid
+  end
+
+  it "is invalid without title" do
+    @sheet.title = nil
+    @sheet.valid?
+
+    expect(@sheet.errors[:title]).to include("can't be blank")
+  end
+
+  xdescribe "up to 15 tasks on one sheet" do
+    before do
+      (1..15).each do |i|
+        @sheet.tasks.create(name: "タスク#{ i }")
+      end
+    end
+    
+    it "only 15 tasks" do
+      expect(@sheet).to be_valid
+    end
+    
+    it "over 15 tasks" do
+      expect{ @sheet.tasks.create(name: "タスク16") }.to raise_error StandardError
+    end
+  end
+end
+
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  before do
+    @task = FactoryBot.create(:task)
+  end
+
+  it "is valid" do
+    expect(@task).to be_valid
+  end
+
+  xit "is invalid without name" do
+    @task.name = nil
+    @task.valid?
+
+    expect(@task.errors[:name]).to include("can't be blank")
+  end
+end
+
