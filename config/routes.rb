a608366171cc0bac252d07d8d70f2c5c52fbb7a1
nishide <nishide.h@gmail.com>
2019-02-18 11:54:20 +0900

    Add Devise Userモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root "static_pages#home"
   resources :sheets do
     resources :tasks do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_062548) do
+ActiveRecord::Schema.define(version: 2019_02_18_013650) do
 
   create_table "sheets", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 2018_11_30_062548) do
     t.boolean "select2", default: false, null: false
     t.boolean "select3", default: false, null: false
     t.index ["sheet_id"], name: "index_tasks_on_sheet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
