be2a931a2932806c6534b73099f940f5eef25b66
nishide <nishide.h@gmail.com>
2019-02-24 11:45:02 +0900

    Fix HerokuのDB:migratreエラー

--- db/migrate/20190220053052_add_user_id_to_sheets.rb
+++ db/migrate/20190220053052_add_user_id_to_sheets.rb
@@ -1,6 +1,5 @@
 class AddUserIdToSheets < ActiveRecord::Migration[5.2]
   def up
-    execute 'DELETE FROM sheets;'
     add_reference :sheets, :user, index: true
     change_column :sheets, :user_id, :integer, null: false
   end
