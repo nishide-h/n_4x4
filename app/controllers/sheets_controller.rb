0c2d060e1caf2e7dac93ebb1407ea027cbccbc1b
nishide <nishide.h@gmail.com>
2019-08-28 16:53:05 +0900

    Remove #63 flashでのメッセージ表示

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -11,37 +11,6 @@ class SheetsController < ApplicationController
 
   def show
     @tasks = @sheet.tasks.order(:created_at)
-
-    if @tasks.size < 15
-      @active_mode = 0
-      case @tasks.size
-      when 1
-        flash[:notice] = "3分以内に全て埋めてしまいましょう！"
-      when 10
-        flash[:notice] = "残り５つ！！"
-      else
-        flash[:notice] = "次々空欄を埋めて行きましょう！！優先順位を決めるのは全て埋めてから。"
-      end
-    else
-      if @tasks.where(select1: true).size < 3
-        if @tasks.where(select1: true).size == 2
-          flash[:notice] = "残りひとつです！！"
-        else
-          flash[:notice] = "書き上げたタスクを俯瞰して、特に重要なものを３つ選択して下さい。"
-        end
-        @active_mode = 1
-      elsif @tasks.where(select2: true).size < 3
-        if @tasks.where(select2: true).size == 2
-          flash[:notice] = "残りひとつです！！"
-        else
-          flash[:notice] = "再び全体を見渡して、緊急性の高いものを３つ選択して下さい。重要と被ってもOK"
-        end
-        @active_mode = 2
-      else
-        flash[:notice] = "もう一度見渡して、人に頼めるものや今日する必要のないタスクを選択して下さい。個数制限なし選択が終われば完成ボタンを押して下さい。"
-        @active_mode = 3
-      end
-    end
   end
 
   def new
@@ -121,3 +90,4 @@ class SheetsController < ApplicationController
       params.require(:sheet).permit(:title)
     end
 end
+
