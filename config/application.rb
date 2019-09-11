d360d58a7a3b68c4311f6adea3b86e69bf52ca6a
nishide <nishide.h@gmail.com>
2019-09-11 23:46:31 +0900

    Fix #70 Timeゾーンが指定されていなかったため日付がずれて表示されていた

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module N4x4
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = "Asia/Tokyo"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
