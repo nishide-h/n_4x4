b67e4bc2178b444a6dc2974c1748c64853584d7a
nishide <nishide.h@gmail.com>
2019-09-30 23:30:41 +0900

    Add #55 Selectqの選択数によるエラー追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,11 +5,16 @@ class Task < ApplicationRecord
 
   validates :name, presence: true
   validate :check_number_of_tasks
+  validate :select1_number
 
   private
     def check_number_of_tasks
       if self.sheet && self.sheet.tasks.count > 15
         errors.add(:sheet, "1シートのタスク数は15個までです。")
+
+    def select1_number
+      if self.sheet && self.sheet.tasks.where(select1: true).size >= 3
+        errors.add(:select1, "選択数は3個までです。")
       end
     end
 end
